9e28704729d71bac6320fc0c9900054680afbde6
root <3626@holbertonschool.com>
2022-03-21 09:50:20 -0700

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,26 +3,28 @@
 start flask web app
 """
 
-
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
-if __name__ == "__main__":
-    app = Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """displays 'Hello HBNB!'"""
+    """
+    states list
+    """
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(cont):
-    """remove the current sql alchemy session"""
+    """
+    remove the current sql alchemy session
+    """
     storage.close()
 
-app.run("0.0.0.0")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
